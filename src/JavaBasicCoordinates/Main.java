a635165599dc597c70c4c0851df21a3b4f3cd095
stolyark <stolyaruk@gmail.com>
2022-09-13 21:24:01 +0300

    Updated code in both classes: Main.java and Parallelepiped.java

--- src/JavaBasicCoordinates/Main.java
+++ src/JavaBasicCoordinates/Main.java
@@ -7,7 +7,8 @@ public class Main {
         double latitude = 50.12979482290925;
         double longitude = 30.65759618278091;
 
-        System.out.println("My coordinates are: \n" +"Latitude - "+ latitude + "\n" + "Longitude - "+ longitude);
+        System.out.println("My coordinates are: \n" +"Latitude - "+ latitude + "\n" + "Longitude - "+ longitude + "\n");
+        System.out.println("Мої координати: \n" +"Широта - "+ latitude + "\n" + "Довгота - "+ longitude);
     }
 }
 
