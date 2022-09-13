254ec0e57a87baf409adae7ecdff075227e89e17
stolyark <stolyaruk@gmail.com>
2022-09-13 20:09:52 +0300

    Added homework code about coordinates

--- src/JavaBasicCoordinates/Main.java
+++ src/JavaBasicCoordinates/Main.java
@@ -3,6 +3,10 @@ package JavaBasicCoordinates;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hellow world!");
+
+        double latitude = 50.12979482290925;
+        double longitude = 30.65759618278091;
+
+        System.out.println("My coordinates are: \n" +"Latitude - "+ latitude + "\n" + "Longitude - "+ longitude);
     }
 }
