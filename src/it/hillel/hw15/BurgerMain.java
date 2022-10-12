eb441626139b8ce10dd2ac4b08dce4380391df21
stolyark <stolyaruk@gmail.com>
2022-10-12 14:24:36 +0300

    Added HW15.

---
+++ src/it/hillel/hw15/BurgerMain.java
@@ -0,0 +1,20 @@
+package it.hillel.hw15;
+
+public class BurgerMain {
+
+    public static void main(String[] args) {
+
+        System.out.print("Бургер №1 - склад: ");
+        Burger burger1 = new Burger("булка", "м'ясо", "сир", "зелень", "майонез");
+        System.out.print("Бургер №2 - склад: ");
+        Burger burger2 = new Burger("булка", "м'ясо", "сир", "зелень");
+        System.out.print("Бургер №3 - склад: ");
+        Burger burger3 = new Burger("булка", "подвійне м'ясо", "сир", "зелень", "майонез");
+        burger3.meat = "подвійне м'ясо";
+
+
+//        System.out.println(burger1);
+//        System.out.println(burger2);
+
+    }
+}
