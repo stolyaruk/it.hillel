0bf5ef21a8e53e9fe62b443c241deb5777e70cff
stolyark <stolyaruk@gmail.com>
2022-10-19 09:41:22 +0300

    HW9. Changed the code to 'while' construction according to the comment from the comment from the tutor.

--- src/it/hillel/hw9/HW9Main.java
+++ src/it/hillel/hw9/HW9Main.java
@@ -10,20 +10,17 @@ public class HW9Main {
         Scanner scanner = new Scanner(System.in);
 
         int secretNumber = randomNumber.nextInt(10);
+        System.out.println("Guess a number: ");
+        int userNumber = scanner.nextInt();
+        int counter = 3;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Guess a number: ");
-            int userNumber = scanner.nextInt();
-
-            if (userNumber == secretNumber) {
-                System.out.println("You win! The secret number was " + secretNumber);
-                break;
-
-            } else {
+        while (counter >= 3) {
+            if (userNumber != secretNumber) {
                 System.out.println("You lost! Try again.  The secret number was " + secretNumber);
-
+            } else {
+                System.out.println("You win! The secret number was " + secretNumber);
             }
+            counter--;
         }
-
     }
 }
